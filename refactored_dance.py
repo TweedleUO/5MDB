04a6ad2f9031554ed1cb5f51cf114301f3b33abd
TweedleUO <em.montero@gmail.com>
2021-06-28 15:26:05 -0700

    reformat code

--- refactored_dance.py
+++ refactored_dance.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-import glob, shutil, random, pygame
-from gpiozero import Button
-from time import sleep
+import glob
+import pygame
+import random
+import shutil
 from os import path
+from time import sleep
+
+from gpiozero import Button
 
 pygame.mixer.init()
 button = Button(10)
@@ -14,6 +18,7 @@ playlist = None
 media_status = None
 i = 0
 
+
 def check_removable_media():
     r_playlist = glob.glob('/media/pi/*/5MDB/playlist/')
     r_played = glob.glob('/media/pi/*/5MDB/played/')
@@ -21,17 +26,19 @@ def check_removable_media():
         return True
     return False
 
+
 def build_playlist(upcoming=song_folder):
     pl = [mp3 for mp3 in glob.glob(upcoming + "*.mp3", recursive=True)]
     random.shuffle(pl)
     return pl
+
 
 def store_path():
     global song_folder
     global played_folder
     validator = song_folder
     if check_removable_media():
-        #extra variable to account for multiple found removable media entries
+        # extra variable to account for multiple found removable media entries
         s_folder = glob.glob('/media/pi/*/5MDB/playlist/')
         song_folder = s_folder[0]
         p_folder = glob.glob('/media/pi/*/5MDB/played/')
@@ -41,10 +48,11 @@ def store_path():
         played_folder = '/home/pi/5MDB/played/'
     if validator == song_folder:
         print('No Change' + song_folder)
-        return 
+        return
     else:
         print('Updated' + str(song_folder))
         return
+
 
 def location_handler():
     global song_folder
@@ -67,22 +75,24 @@ def location_handler():
     print('media source all good')
     return
 
+
 def load_play():
     global i
     global playlist
     global played_folder
-    print('Now playing:',what_song(i))
+    print('Now playing:', what_song(i))
     try:
         pygame.mixer.music.load(playlist[i])
         pygame.mixer.music.play()
-        shutil.move(playlist[i],played_folder)
+        shutil.move(playlist[i], played_folder)
         return
     except IndexError:
         reset_played_dir()
         location_handler()
         pygame.mixer.music.load(playlist[i])
         pygame.mixer.music.play()
-        shutil.move(playlist[i],played_folder)
+        shutil.move(playlist[i], played_folder)
+
 
 def what_song(index):
     global playlist
@@ -90,13 +100,15 @@ def what_song(index):
     this_song = playlist[index]
     return path.basename(this_song.strip('.mp3'))
 
+
 def reset_played_dir():
     global song_folder
     global played_folder
     directory = [mp3 for mp3 in glob.glob(played_folder + "*.mp3", recursive=True)]
     for file in directory:
-        shutil.move(file,song_folder)
+        shutil.move(file, song_folder)
     return
+
 
 def dance_break():
     global i
@@ -123,21 +135,20 @@ def dance_break():
             reset_played_dir()
             location_handler()
             load_play()
-            
+
     i += 1
-    if len(playlist)-i == 0:
+    if len(playlist) - i == 0:
         reset_played_dir()
         playlist = build_playlist()
         i = 0
-        print('playlist reset Next song:',what_song(i))
-        print(len(playlist)-i, 'songs left in playlist')
+        print('playlist reset Next song:', what_song(i))
+        print(len(playlist) - i, 'songs left in playlist')
     else:
-        print('Next song:',what_song(i))
-        print(len(playlist)-i, 'songs left in playlist')
+        print('Next song:', what_song(i))
+        print(len(playlist) - i, 'songs left in playlist')
     return
 
+
+# noinspection PyRedeclaration
 playlist = build_playlist()
 button.when_pressed = dance_break
-
-        
-    
